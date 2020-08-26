30421d94acc540b2c45094b12fd6e8c5e682c169
William De Souza Floriano <jesus.wsf123@gmail.com>
2020-08-26 10:40:34 -0300

    Conclusão

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository does not exist." });
+  }
+
+  const repo = {
+    id,
+    url,
+    title,
+    techs,
+    likes: repositories[repoIndex].likes,
+  };
+
+  repositories[repoIndex] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository does not exist." });
+  }
+  repositories.splice(repositoryIndex);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository does not exist." });
+  }
+
+  const repo = {
+    id,
+    url: repositories[repositoryIndex].url,
+    title: repositories[repositoryIndex].title,
+    techs: repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes + 1,
+  };
+
+  repositories[repositoryIndex] = repo;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
